c856b82c66f48a1a8a51f729f4eb49c71ce77746
Rafał <rafal@xxx.xx>
2018-07-20 12:36:10 +0200

    ok

--- src/main/java/rest_api_glupi_klient1/rest_service/MadryController.java
+++ src/main/java/rest_api_glupi_klient1/rest_service/MadryController.java
@@ -1,18 +1,18 @@
 package rest_api_glupi_klient1.rest_service;
 
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope
+@RefreshScope
 @RestController
 public class MadryController {
 
-	@Value("${message}")
+    @Value("${message:Hello default}")
 	private String messageFromConfig;
 	
 	@RequestMapping(value="/eloConfig", method=RequestMethod.GET)
