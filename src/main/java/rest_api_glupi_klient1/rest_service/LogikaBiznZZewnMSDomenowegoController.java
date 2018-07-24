88d6db7c9001d5ed2b8bb9e5d4411cb06920d810
Rafał <rafal@xxx.xx>
2018-07-24 09:25:46 +0200

    dziala corelationid

--- src/main/java/rest_api_glupi_klient1/rest_service/LogikaBiznZZewnMSDomenowegoController.java
+++ src/main/java/rest_api_glupi_klient1/rest_service/LogikaBiznZZewnMSDomenowegoController.java
@@ -5,6 +5,8 @@ import java.net.URISyntaxException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +21,23 @@ public class LogikaBiznZZewnMSDomenowegoController {
 
 	private static final Log log= LogFactory.getLog(LogikaBiznZZewnMSDomenowegoController.class);
 	
+	   @Autowired
+	    private RestTemplate restTemplate;
+	   /**
+	    * Bardzo ważne jest oznaczenie RestTemplate jako @Bean ponieważ wtedy dodawany jest do niego interceptor odpowiedzialny za rozproszone logowanie,
+	    *  bez tego, nie otrzymamy pożądanego efektu. 
+	    * @return
+	    */
+	  @Bean
+	    public RestTemplate getRestTemplate() {
+	        return new RestTemplate();
+	    }
+	
 	@RequestMapping(value="/domainLogic", method=RequestMethod.GET)
 	public String callexternalDomainMS() throws URISyntaxException{
 		log.info(LogikaBiznZZewnMSDomenowegoController.class.getName() +  " wywolywanie logiki biz");
-		RestTemplate rt = new RestTemplate();
 		URI uri = new URI("http://localhost:8091/logic/");
 		//!!! przeniesc do konfiguracji
-		return rt.getForObject(uri, String.class);
+		return restTemplate.getForObject(uri, String.class);
 	}
 }
